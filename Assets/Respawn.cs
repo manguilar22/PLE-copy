6574dad7d0b5981459cde0320de363291953ca39
manguilar22 <manguilar22@gmail.com>
2020-09-16 19:51:10 -0600

    project dump

---
+++ Assets/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject Spawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            transform.position = Spawn.transform.position;
+        }
+        
+    }
+}
